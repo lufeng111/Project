574a5ab843f2251f6fdf4dbe0e309f341d5b08fa
Feng F LU <flu038@nam.ad.pwcinternal.com>
2019-08-06 17:43:17 +0800

    服务

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,7 @@
 import { Hero } from './heroes/hero';
 import { HEROES } from './mock-heroes';
 import { Injectable } from '@angular/core';
+import {Observable, of} from 'rxjs'
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +10,12 @@ export class HeroService {
 
   constructor() { }
 
-  getHeros(): Hero[] {
-    return HEROES;
+  // getHeros(): Hero[] {
+  //   return HEROES;
+  // }
+  getHeroes(): Observable<Hero[]>{
+    return of(HEROES)
   }
+
 
 }
