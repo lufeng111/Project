54df052e8c408f0ddfbc58a361d8a075a99f27f7
Feng F LU <flu038@nam.ad.pwcinternal.com>
2019-08-08 18:02:24 +0800

    http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
+import { InMemoryDataService } from './in-memory-data.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import {FormsModule} from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
-import { DashboardComponent } from './dashboard/dashboard.component'
+import { DashboardComponent } from './dashboard/dashboard.component';
+import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
 
 @NgModule({
   declarations: [
@@ -21,6 +23,10 @@ import { DashboardComponent } from './dashboard/dashboard.component'
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, {dataEncapsulation: false}
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,37 +1,64 @@
 import { Hero } from './heroes/hero';
-import { HEROES } from './mock-heroes';
+// import { HEROES } from './mock-heroes';
 import { Injectable } from '@angular/core';
-import {Observable, of} from 'rxjs';
+import {Observable, of, from} from 'rxjs';
 import {MessageService} from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {catchError, map, tap} from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
   // 这是典型的服务注入服务
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+    ) { }
+
+    private log(message: string) {
+      this.messageService.add(`HeroService: ${message}`);
+    }
+    private heroesUrl = 'api/heroes';
+
+
 
   // getHeros(): Hero[] {
   //   return HEROES;
   // }
+  // 把 getHeroes（）方法转换成下面HttpClient
   // getHeroes(): Observable<Hero[]>{
   //   return of(HEROES)
   // }
+  getHeroes(): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.heroesUrl).pipe(
+      catchError(this.handleError<Hero[]>('getHeroes', []))
+    );
+  }
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      this.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    }
+  }
+
+
 
   // getHeroes(): Observable<Hero[]>{
   //   this.messageService.add('HeroService: fetched heroes');
   //   return of(HEROES);
   // }
 
-  getHeroes(): Observable<Hero[]> {
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
-  }
+  // getHeroes(): Observable<Hero[]> {
+  //   this.messageService.add('HeroService: fetched heroes');
+  //   return of(HEROES);
+  // }
 
-  getHero(id: number): Observable<Hero> {
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
-  }
+  // getHero(id: number): Observable<Hero> {
+  //   this.messageService.add(`HeroService: fetched hero id=${id}`);
+  //   return of(HEROES.find(hero => hero.id === id));
+  // }
 
 
 }
